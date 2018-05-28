66440d04251652eed7380066d990ad23c5359a0a
Uros Trstenjak <uros.trstenjak@gmail.com>
2018-05-28 22:07:48 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: MIT License',
+        'License :: OSI Approved :: MIT License',
 	'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
