6290118d1c91febe2076ff0b55d11c9763ef2a17
Uros Trstenjak <uros.trstenjak@gmail.com>
2018-08-11 20:55:33 +0200

    fix #2

--- bootstrap_modal_forms/static/js/jquery.bootstrap.modal.forms.js
+++ bootstrap_modal_forms/static/js/jquery.bootstrap.modal.forms.js
@@ -1,6 +1,6 @@
 /*
 django-bootstrap-modal-forms
-version : 1.0
+version : 1.1.0
 Copyright (c) 2018 Uros Trstenjak
 https://github.com/trco/django-bootstrap-modal-forms
 */
@@ -8,16 +8,16 @@ https://github.com/trco/django-bootstrap-modal-forms
 (function ($) {
 
     // Place the form at formURL to modalContent element of modal with id=modalID
-    var newForm = function (modalContent, formURL, modalID, modalForm, errorClass) {
+    var newForm = function (modalID, modalContent, modalForm, formURL, successURL, errorClass) {
         $(modalContent).load(formURL, function () {
             $(modalID).modal('toggle');
-            ajaxSubmit(modalForm, modalID, modalContent, errorClass);
+            ajaxSubmit(modalID, modalContent, modalForm, successURL, errorClass);
         });
     };
 
     // Add AJAX validation to the modalForm
-    var ajaxSubmit = function (form, modal, modalContent, errorClass) {
-        $(form).submit(function (event) {
+    var ajaxSubmit = function (modalID, modalContent, modalForm, successURL, errorClass) {
+        $(modalForm).submit(function (event) {
             // Prevent submit and POST form to url using AJAX
             event.preventDefault();
             $.ajax({
@@ -29,13 +29,13 @@ https://github.com/trco/django-bootstrap-modal-forms
                     // Update form with errors after unsuccessful POST request
                     // Django form.is_valid() = False
                     if ($(response).find(errorClass).length > 0) {
-                        $(modal).find(modalContent).html(response);
-                        ajaxSubmit(form, modal, modalContent, errorClass);
+                        $(modalID).find(modalContent).html(response);
+                        ajaxSubmit(modalID, modalContent, modalForm, successURL, errorClass);
                     }
-                    // Hide modal after successful POST request
-                    // Django form.is_valid() = True
+                    // Hide modal after successful POST request when & redirect to successURL
                     else {
-                        $(modal).modal("hide");
+                        $(modalID).modal("hide");
+                        window.location.href = successURL;
                     }
                 }
             });
@@ -45,10 +45,11 @@ https://github.com/trco/django-bootstrap-modal-forms
     $.fn.modalForm = function (options) {
         // Default settings
         var defaults = {
-            modalContent: ".modal-content",
-            formURL: null,
             modalID: "#modal",
+            modalContent: ".modal-content",
             modalForm: ".modal-content form",
+            formURL: null,
+            successURL: "/",
             errorClass: ".invalid"
         };
 
@@ -57,10 +58,11 @@ https://github.com/trco/django-bootstrap-modal-forms
 
         return this.each(function () {
             $(this).click(function (event) {
-                newForm(settings.modalContent,
-                    settings.formURL,
-                    settings.modalID,
+                newForm(settings.modalID,
+                    settings.modalContent,
                     settings.modalForm,
+                    settings.formURL,
+                    settings.successURL,
                     settings.errorClass);
                 event.preventDefault();
             });
