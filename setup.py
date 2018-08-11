3dade91bc8d65fd2834c4c02127da6fb940cc515
Uros Trstenjak <uros.trstenjak@gmail.com>
2018-08-11 21:19:04 +0200

    prepare for 1.1.0 release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-bootstrap-modal-forms',
-    version='1.0',
+    version='1.1.0',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
@@ -23,7 +23,7 @@ setup(
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-	'Operating System :: OS Independent',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
