ee7c5aef1827e5efdc8a4dcd6e7e08a0bfce11d4
Uros Trstenjak <uros.trstenjak@gmail.com>
2018-08-12 16:14:37 +0200

    prepare for 1.2.0 release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-bootstrap-modal-forms',
-    version='1.1.0',
+    version='1.2.0',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
