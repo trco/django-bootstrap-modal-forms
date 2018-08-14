0ca28dc45190184c07be1201399d42dc512fcbd3
Uros Trstenjak <uros.trstenjak@gmail.com>
2018-08-14 11:21:31 +0200

    fix #5

--- bootstrap_modal_forms/static/js/jquery.bootstrap.modal.forms.js
+++ bootstrap_modal_forms/static/js/jquery.bootstrap.modal.forms.js
@@ -1,6 +1,6 @@
 /*
 django-bootstrap-modal-forms
-version : 1.2.0
+version : 1.2.1
 Copyright (c) 2018 Uros Trstenjak
 https://github.com/trco/django-bootstrap-modal-forms
 */
@@ -10,14 +10,14 @@ https://github.com/trco/django-bootstrap-modal-forms
     // Place the form at formURL to modalContent element of modal with id=modalID
     var newForm = function (modalID, modalContent, modalForm, formURL, successURL, errorClass) {
         $(modalContent).load(formURL, function () {
-            $(modalID).modal('toggle');
-            $(modalForm).attr('action', formURL);
-            ajaxSubmit(modalID, modalContent, modalForm, successURL, errorClass);
+            $(modalID).modal("show");
+            $(modalForm).attr("action", formURL);
+            ajaxSubmit(modalID, modalContent, modalForm, formURL, successURL, errorClass);
         });
     };
 
     // Add AJAX validation to the modalForm
-    var ajaxSubmit = function (modalID, modalContent, modalForm, successURL, errorClass) {
+    var ajaxSubmit = function (modalID, modalContent, modalForm, formURL, successURL, errorClass) {
         $(modalForm).submit(function (event) {
             // Prevent submit and POST form to url using AJAX
             event.preventDefault();
@@ -31,7 +31,8 @@ https://github.com/trco/django-bootstrap-modal-forms
                     // Django form.is_valid() = False
                     if ($(response).find(errorClass).length > 0) {
                         $(modalID).find(modalContent).html(response);
-                        ajaxSubmit(modalID, modalContent, modalForm, successURL, errorClass);
+                        $(modalForm).attr("action", formURL);
+                        ajaxSubmit(modalID, modalContent, modalForm, formURL, successURL, errorClass);
                     }
                     // Hide modal after successful POST request when & redirect to successURL
                     else {
