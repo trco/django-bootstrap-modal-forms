37750f5241a8d44243eb271c7f0ccb5954b7003b
Uros Trstenjak <uros.trstenjak@gmail.com>
2018-08-31 20:49:05 +0200

    fix #7

---
+++ bootstrap_modal_forms/mixins.py
@@ -0,0 +1,65 @@
+from django.contrib import messages
+from django.contrib.auth import (REDIRECT_FIELD_NAME, get_user_model, login
+                                 as auth_login)
+from django.http import HttpResponseRedirect
+
+
+class PassRequestMixin(object):
+    """
+    Mixin which puts the request into the form's kwargs.
+
+    Note: Using this mixin requires you to pop the `request` kwarg
+    out of the dict in the super of your form's `__init__`.
+    """
+    def get_form_kwargs(self):
+        kwargs = super(PassRequestMixin, self).get_form_kwargs()
+        # Update the existing form kwargs dict with the request's user.
+        kwargs.update({"request": self.request})
+        return kwargs
+
+
+class PopRequestMixin(object):
+    """
+    Mixin which pops request out of the kwargs and attaches it to the form's
+    instance.
+
+    Note: This mixin must precede forms.ModelForm/forms.Form. The form is not
+    expecting these kwargs to be passed in, so they must be popped off before
+    anything else is done.
+    """
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop("request", None)
+        super(PopRequestMixin, self).__init__(*args, **kwargs)
+
+
+class CreateUpdateAjaxMixin(object):
+    """
+    Mixin which passes or saves object based on request type.
+    """
+    def save(self, commit=True):
+        if not self.request.is_ajax():
+            instance = super(CreateUpdateAjaxMixin, self).save(commit=commit)
+        else:
+            instance = super(CreateUpdateAjaxMixin, self).save(commit=False)
+        return instance
+
+
+class DeleteAjaxMixin(object):
+    """
+    Mixin which deletes object if request is not ajax request.
+    """
+    def post(self, request, *args, **kwargs):
+        if not request.is_ajax():
+            messages.success(request, self.success_message)
+        return super(DeleteAjaxMixin, self).delete(request, *args, **kwargs)
+
+
+class LoginAjaxMixin(object):
+    """
+    Mixin which authenticates user if request is not ajax request.
+    """
+    def form_valid(self, form):
+        if not self.request.is_ajax():
+            auth_login(self.request, form.get_user())
+            messages.success(self.request, self.success_message)
+        return HttpResponseRedirect(self.get_success_url())
--- bootstrap_modal_forms/static/js/jquery.bootstrap.modal.forms.js
+++ bootstrap_modal_forms/static/js/jquery.bootstrap.modal.forms.js
@@ -1,47 +1,57 @@
 /*
 django-bootstrap-modal-forms
-version : 1.2.1
+version : 1.3.0
 Copyright (c) 2018 Uros Trstenjak
 https://github.com/trco/django-bootstrap-modal-forms
 */
 
 (function ($) {
 
-    // Place the form at formURL to modalContent element of modal with id=modalID
-    var newForm = function (modalID, modalContent, modalForm, formURL, successURL, errorClass) {
+    // Open modal & load the form at formURL to the modalContent element
+    var newForm = function (modalID, modalContent, modalForm, formURL, errorClass, submitBtn) {
         $(modalContent).load(formURL, function () {
             $(modalID).modal("show");
             $(modalForm).attr("action", formURL);
-            ajaxSubmit(modalID, modalContent, modalForm, formURL, successURL, errorClass);
+            // Add click listener to the submitBtn
+            ajaxSubmit(modalID, modalContent, modalForm, formURL, errorClass, submitBtn);
         });
     };
 
-    // Add AJAX validation to the modalForm
-    var ajaxSubmit = function (modalID, modalContent, modalForm, formURL, successURL, errorClass) {
-        $(modalForm).submit(function (event) {
-            // Prevent submit and POST form to url using AJAX
-            event.preventDefault();
-            $.ajax({
-                type: $(this).attr("method"),
-                url: $(this).attr("action"),
-                // Serialize form data
-                data: $(this).serialize(),
-                success: function (response) {
-                    // Update form with errors after unsuccessful POST request
-                    // Django form.is_valid() = False
-                    if ($(response).find(errorClass).length > 0) {
-                        $(modalID).find(modalContent).html(response);
-                        $(modalForm).attr("action", formURL);
-                        ajaxSubmit(modalID, modalContent, modalForm, formURL, successURL, errorClass);
-                    }
-                    // Hide modal after successful POST request when & redirect to successURL
-                    else {
-                        $(modalID).modal("hide");
-                        window.location.href = successURL;
-                    }
-                }
-            });
+    // Add click listener to the submitBtn
+    var ajaxSubmit = function (modalID, modalContent, modalForm, formURL, errorClass, submitBtn) {
+        $(submitBtn).on("click", function () {
+            // Check if form.is_valid() via ajax request
+            var formIsValid = isFormValid(modalID, modalContent, modalForm, formURL, errorClass);
+            if (formIsValid) {
+                // Submit form if form.is_valid()
+                $(modalForm).submit();
+            } else {
+                // Reinstantiate click listener on submitBtn
+                // Form is updated with errors in isFormValid(...) call
+                ajaxSubmit(modalID, modalContent, modalForm, formURL, errorClass, submitBtn);
+            }
         });
+    };
+
+    // Check if form.is_valid()
+    var isFormValid = function (modalID, modalContent, modalForm, formURL, errorClass) {
+        var formIsValid = true;
+        $.ajax({
+            type: $(modalForm).attr("method"),
+            url: $(modalForm).attr("action"),
+            async: false,
+            // Serialize form data
+            data: $(modalForm).serialize(),
+            success: function (response) {
+                if ($(response).find(errorClass).length > 0) {
+                    formIsValid = false;
+                    // Update form with errors if not form.is_valid()
+                    $(modalID).find(modalContent).html(response);
+                    $(modalForm).attr("action", formURL);
+                }
+            }
+        });
+        return formIsValid;
     };
 
     $.fn.modalForm = function (options) {
@@ -51,22 +61,23 @@ https://github.com/trco/django-bootstrap-modal-forms
             modalContent: ".modal-content",
             modalForm: ".modal-content form",
             formURL: null,
-            successURL: "/",
-            errorClass: ".invalid"
+            errorClass: ".invalid",
+            submitBtn: ".submit-btn"
         };
 
         // Extend default settings with provided options
         var settings = $.extend(defaults, options);
 
         return this.each(function () {
+            // Add click listener to the element with attached modalForm
             $(this).click(function (event) {
+                // Instantiate new modalForm in modal
                 newForm(settings.modalID,
                     settings.modalContent,
                     settings.modalForm,
                     settings.formURL,
-                    settings.successURL,
-                    settings.errorClass);
-                event.preventDefault();
+                    settings.errorClass,
+                    settings.submitBtn);
             });
         });
     };
