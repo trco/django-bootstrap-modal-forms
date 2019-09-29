f5888438ab7385dd5d07227d52c02aa31bef2584
trco <uros.trstenjak@gmail.com>
2019-09-29 14:49:09 +0200

    add support for Django development versions

--- bootstrap_modal_forms/generic.py
+++ bootstrap_modal_forms/generic.py
@@ -4,14 +4,15 @@ from django.views import generic
 from .mixins import PassRequestMixin, DeleteMessageMixin, LoginAjaxMixin
 
 DJANGO_VERSION = django.get_version().split('.')
-DJANGO_MAJOR_VERSION = int(DJANGO_VERSION[0])
-DJANGO_MINOR_VERSION = int(DJANGO_VERSION[1])
+DJANGO_MAJOR_VERSION = DJANGO_VERSION[0];
+DJANGO_MINOR_VERSION = DJANGO_VERSION[1];
 
-# Import custom LoginView for Django versions < 1.11 
-if DJANGO_MAJOR_VERSION == 1 and DJANGO_MINOR_VERSION < 11:
+# Import custom LoginView for Django versions < 1.11
+if DJANGO_MAJOR_VERSION == '1' and '11' not in DJANGO_MINOR_VERSION:
     from .compatibility import LoginView
 else:
     from django.contrib.auth.views import LoginView
+
 
 class BSModalLoginView(LoginAjaxMixin, SuccessMessageMixin, LoginView):
     pass
