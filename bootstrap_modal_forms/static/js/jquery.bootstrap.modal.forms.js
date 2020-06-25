aa70ec93e5d68aece976e2c20269813046a15bc4
trco <uros.trstenjak@gmail.com>
2020-06-25 23:32:25 +0200

    refactor js and add functionality to create or update without redirect and with opened modal

--- bootstrap_modal_forms/static/js/jquery.bootstrap.modal.forms.js
+++ bootstrap_modal_forms/static/js/jquery.bootstrap.modal.forms.js
@@ -1,7 +1,7 @@
 /*
 django-bootstrap-modal-forms
-version : 1.5.0
-Copyright (c) 2019 Uros Trstenjak
+version : 2.0.0
+Copyright (c) 2020 Uros Trstenjak
 https://github.com/trco/django-bootstrap-modal-forms
 */
 
@@ -28,8 +28,42 @@ https://github.com/trco/django-bootstrap-modal-forms
     };
 
     // Submit form callback function
-    var submitForm = function (modalForm) {
-        $(modalForm).submit();
+    var submitForm = function (settings) {
+        if (settings.closeOnSubmit) {
+            $(settings.modalForm).submit();
+        } else {
+            $.ajax({
+                type: $(settings.modalForm).attr("method"),
+                url: $(settings.modalForm).attr("action"),
+                // Add closeOnSubmit and check for it in save method of CreateUpdateAjaxMixin
+                data: $(settings.modalForm).serialize() + "&closeOnSubmit=False",
+                success: function (response) {
+                    $(settings.modalID).prepend(settings.ajaxSuccessMessage);
+
+                    // Update page without refresh
+                    $.ajax({
+                        type: "GET",
+                        url: settings.dataUrl,
+                        dataType: "json",
+                        success: function (response) {
+                            // Reload form
+                            $(settings.modalID).find(settings.modalContent).load(settings.formURL, function () {
+                                $(settings.modalForm).attr("action", settings.formURL);
+                                addEventHandlers(settings);
+                            });
+
+                            // Update page
+                            $(settings.dataElementId).html(response[settings.dataKey]);
+                            
+                            // Add modalForm to trigger element after ajax page update
+                            if (settings.addModalFormFunction) {
+                                settings.addModalFormFunction();
+                            }
+                        }
+                    });
+                }
+            });
+        }
     };
 
     // Check if form.is_valid() & either show errors or submit it via callback
@@ -50,7 +84,7 @@ https://github.com/trco/django-bootstrap-modal-forms
                     addEventHandlers(settings);
                 } else {
                     // Form is valid, submit it
-                    callback(settings.modalForm);
+                    callback(settings);
                 }
             }
         });
@@ -64,7 +98,13 @@ https://github.com/trco/django-bootstrap-modal-forms
             modalForm: ".modal-content form",
             formURL: null,
             errorClass: ".invalid",
-            submitBtn: ".submit-btn"
+            submitBtn: ".submit-btn",
+            closeOnSubmit: true,
+            ajaxSuccessMessage: null,
+            dataUrl: null,
+            dataElementId: null,
+            dataKey: null,
+            addModalFormFunction: null
         };
 
         // Extend default settings with provided options
