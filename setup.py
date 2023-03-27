560cf8e393c56e8c14ef890d1ab85292e6a2f995
MBoaretto25 <marco.boaretto19@gmail.com>
2023-03-27 10:59:40 -0300

    update tag version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-bootstrap-modal-forms',
-    version='master',
+    version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
     license='MIT License',
